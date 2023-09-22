9ba31dfa7e3e241003536e2dbd176ed07651c879
erik <erikbrito01@gmail.com>
2023-09-22 13:09:51 -0300

    test: depploy v3

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middleware = jsonServer.defaults()
+const port = process.env.PORT || 5000
+
+server.use(middleware)
+server.use(router)
+
+console.log(`to open: http://localhost:5000
+
+`)
+console.log(`http://localhost:5000/products`)
+console.log(`http://localhost:5000/categories`)
+server.listen(port)
